3427558ed7fed81807704a5ec8ceac94c73e6706
fasta <fastakovskiy.mihail04@gmail.com>
2025-04-25 16:29:13 +0300

    add validation

--- NetCsharpMVC/Models/Film.cs
+++ NetCsharpMVC/Models/Film.cs
@@ -1,14 +1,37 @@
-﻿namespace NetCsharpMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetCsharpMVC.Models
 {
     public class Film
     {
       
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Поле должно быть установлено.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Длина строки должна быть от 3 до 50 символов")]
+        [Display(Name = "Название")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Поле должно быть установлено.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Длина строки должна быть от 3 до 50 символов")]
+        [RegularExpression(@"^[A-Za-z\s]+$", ErrorMessage = "Разрешены только буквы")]
+
+        [Display(Name = "Режиссер")]
         public string Director { get; set; }
+        [Required(ErrorMessage = "Поле должно быть установлено.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Длина строки должна быть от 3 до 50 символов")]
+        [RegularExpression(@"^[A-Za-z\s]+$", ErrorMessage = "Разрешены только буквы")]
+
+        [Display(Name = "Жанр")]
         public string Genr { get; set; }
+        [Required]
+        [Display(Name = "Год")]
+        [Range(1895, int.MaxValue, ErrorMessage = "Введите число не меньше 1895")]
         public int Year {  get; set; }
         public string Poster {  get; set; }
+
+        [Required(ErrorMessage = "Поле должно быть установлено.")]
+        [StringLength(500, MinimumLength = 3, ErrorMessage = "Длина строки должна быть от 3 до 500 символов")]
+        [Display(Name = "Описание")]
         public string Description { get; set; } 
     }
 }
